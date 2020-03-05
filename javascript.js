2f424118d5123ff6b255b3462cbaa90e401a6c81
simonesciacovelli <scias758@newschool.edu>
2020-03-05 17:39:24 -0500

    df

--- javascript.js
+++ javascript.js
@@ -72,6 +72,15 @@
 
 AOS.init();
 
+const loadingManager = new THREE.LoadingManager( () => {
+
+    const loadingScreen = document.getElementById( 'loading-screen' );
+    loadingScreen.classList.add( 'fade-out' );
+
+    // optional: remove loader from DOM via event listener
+    loadingScreen.addEventListener( 'transitionend', onTransitionEnd );
+
+} );
 
 $("#arrowdown").click(function() {
     $('html, body').animate({

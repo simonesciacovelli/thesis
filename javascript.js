c84bcbd02946808ba6b1c191dfb2a73978b8f01d
simonesciacovelli <scias758@newschool.edu>
2020-03-10 21:53:18 -0400

    wd

--- javascript.js
+++ javascript.js
@@ -1,27 +1,3 @@
-function touchHandler(event) {
-    var touch = event.changedTouches[0];
-
-    var simulatedEvent = document.createEvent("MouseEvent");
-        simulatedEvent.initMouseEvent({
-        touchstart: "mousedown",
-        touchmove: "mousemove",
-        touchend: "mouseup"
-    }[event.type], true, true, window, 1,
-        touch.screenX, touch.screenY,
-        touch.clientX, touch.clientY, false,
-        false, false, false, 0, null);
-
-    touch.target.dispatchEvent(simulatedEvent);
-    event.preventDefault();
-}
-
-function init() {
-    document.addEventListener("touchstart", touchHandler, true);
-    document.addEventListener("touchmove", touchHandler, true);
-    document.addEventListener("touchend", touchHandler, true);
-    document.addEventListener("touchcancel", touchHandler, true);
-}
-
 AOS.init();
 
 const loadingManager = new THREE.LoadingManager( () => {
@@ -40,10 +16,6 @@ $("#arrowdown").click(function() {
     }, 1000);
 });
 
-
-  $( function() {
-    $( ".contextmenu" ).draggable();
-  } );
 
 Chart.Legend.prototype.afterFit = function() {
     this.height = this.height + 50;

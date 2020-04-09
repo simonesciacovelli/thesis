97cac43da10969d5d37619a2c2cc3d945aac2c7a
simonesciacovelli <scias758@newschool.edu>
2020-04-09 18:10:57 +0200

    we

--- javascript.js
+++ javascript.js
@@ -7,11 +7,65 @@ AOS.init();
 // });
 
 
-$('.plybtn').click(function(){
+$('.imgcont_1, .plybtn_1').click(function(){
   $('#my-video_1').get(0).play()
+  $(".imgcont_1").css('border', 'solid #404040 9px');
+  $(".plybtn_1").css('backgroundColor', '#404040');
 });
 
+$('.imgcont_2, .plybtn_2').click(function(){
+  $('#my-video_2').get(0).play()
+  $(".imgcont_2").css('border', 'solid #404040 9px');
+  $(".plybtn_2").css('backgroundColor', '#404040');
+});
 
+$('.imgcont_3, .plybtn_3').click(function(){
+  $('#my-video_3').get(0).play()
+  $(".imgcont_3").css('border', 'solid #404040 9px');
+  $(".plybtn_3").css('backgroundColor', '#404040');
+});
+
+$('.imgcont_4, .plybtn_4').click(function(){
+  $('#my-video_4').get(0).play()
+  $(".imgcont_4").css('border', 'solid #404040 9px');
+  $(".plybtn_4").css('backgroundColor', '#404040');
+});
+
+$('.imgcont_5, .plybtn_5').click(function(){
+  $('#my-video_5').get(0).play()
+  $(".imgcont_5").css('border', 'solid #404040 9px');
+  $(".plybtn_5").css('backgroundColor', '#404040');
+});
+
+$('.imgcont_6, .plybtn_6').click(function(){
+  $('#my-video_6').get(0).play()
+  $(".imgcont_6").css('border', 'solid #404040 9px');
+  $(".plybtn_6").css('backgroundColor', '#404040');
+});
+
+$('.imgcont_7, .plybtn_7').click(function(){
+  $('#my-video_7').get(0).play()
+  $(".imgcont_7").css('border', 'solid #404040 9px');
+  $(".plybtn_7").css('backgroundColor', '#404040');
+});
+
+$('.imgcont_8, .plybtn_8').click(function(){
+  $('#my-video_8').get(0).play()
+  $(".imgcont_8").css('border', 'solid #404040 9px');
+  $(".plybtn_8").css('backgroundColor', '#404040');
+});
+
+$('.imgcont_9, .plybtn_9').click(function(){
+  $('#my-video_9').get(0).play()
+  $(".imgcont_9").css('border', 'solid #404040 9px');
+  $(".plybtn_9").css('backgroundColor', '#404040');
+});
+
+$('.imgcont_10, .plybtn_10').click(function(){
+  $('#my-video_10').get(0).play()
+  $(".imgcont_10").css('border', 'solid #404040 9px');
+  $(".plybtn_10").css('backgroundColor', '#404040');
+});
 
 $('[data-sidenav]').sidenav();
 
@@ -81,7 +135,17 @@ function isInView(el) {
 // });
 
 $(window).scroll(function() {
-    $('.imgcont').each(function() {
+    $("div[class^='imgcont']").each(function() {
+        if(isInView(this)) {
+               $(this).css('opacity', '1');
+        } else {
+                $(this).css('opacity', '0');
+        }
+    });
+});
+
+$(window).scroll(function() {
+    $('.plybtn_1, .plybtn_2, .plybtn_3, .plybtn_4, .plybtn_5, .plybtn_6, .plybtn_7, .plybtn_8, .plybtn_9, .plybtn_10').each(function() {
         if(isInView(this)) {
                $(this).css('opacity', '1');
         } else {
